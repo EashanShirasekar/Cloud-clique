d5f0e6e6d995190126150485983055178ca43d69
EashanShirasekar <eashanshirsekar137@gmail.com>
2025-03-16 16:39:02 +0530

    inital

---
+++ config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+async function connectDB() {
+  try {
+    // Database connection
+    await mongoose.connect(process.env.MONGO_CONNECTION_URL); 
+
+    console.log('Database connected.');
+  } catch (err) {
+    console.error('Connection failed:', err);
+  }
+}
+
+module.exports = connectDB;
