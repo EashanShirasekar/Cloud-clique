0d6f9c59b9bcc0c3123d9f99c490a7fe198646d5
EashanShirasekar <eashanshirsekar137@gmail.com>
2025-03-16 17:10:20 +0530

    added cors

---
+++ functions/uploads/1741876189047-532287586.java
@@ -0,0 +1,12 @@
+public class Arithmetic1
+{
+  public static void main(String args[])
+    {
+      int a = Integer.parseInt(args[0]);
+      int b = Integer.parseInt(args[1]);
+      int c = Integer.parseInt(args[2]);
+      int result = (a*b)+c;
+     System.out.println("Result of the Expression is :"+result);
+     }
+
+}
